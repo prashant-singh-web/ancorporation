f578619b2efc547fe2954a01a7254b9a6728b335
prashant-singh-web <142319958+prashant-singh-web@users.noreply.github.com>
2024-02-26 10:29:39 +0530

    Update Home_Page.tsx
    update code for home background change

--- src/app/components/Home_Page.tsx
+++ src/app/components/Home_Page.tsx
@@ -13,16 +13,16 @@ function Home_Page() {
   
 
   return (
-    <div className="w-full min-h-[100vh] relative bg-black/50 duration-300 ease-in-out  grid place-items-center  ">
+    <div className="w-full min-h-[100vh] relative bg-black duration-300 ease-in-out  grid place-items-center  ">
        <div className=" text-white">
         <h1 className="text-8xl text-center font-bold tracking-tight my-auto" data-aos="fade-up">“Your Partner in Pipeline Innovation!”</h1>
         {/* <p className="text-3xl font-semibold mt-2 text-center " data-aos="fade-up">A.N.CORPORATION</p> */}
         <button onClick={scrollToDown} className="size-16 absolute bottom-5 left-1/2 flex flex-row rounded-full border mt-8 text-gray-200/35 hover:text-gray-300/80 hover:bg-white/25 font-semibold duration-200 ease-in-out  group "><FaArrowDown className="text-2xl m-auto group-hover:translate-y-1 duration-300 ease-in-out" /> </button>
        </div>
        <div className="w-full h-full  grid place-items-center">
-        <div className=" bg-white absolute inset-0 -z-10  overflow-hidden">
+{/*         <div className=" bg-white absolute inset-0 -z-10  overflow-hidden">
             <Image src={`/home.jpg`} alt="home" width={100} height={100} unoptimized className="w-full h-full hover:scale-125 duration-300 ease-in-out" />
-        </div>
+        </div> */}
        </div>
     </div>
   );
