9f24651cb0e90609841c2f43402f987cdcfea714
prashant-singh-web <142319958+prashant-singh-web@users.noreply.github.com>
2024-03-03 08:38:18 +0530

    Update route.ts

--- src/app/api/ContactForm/getDetail/route.ts
+++ src/app/api/ContactForm/getDetail/route.ts
@@ -9,31 +9,31 @@ connectDB();
 
 export async function GET(req: NextRequest, res: NextResponse) {
   try {
-      const searchQuery = req.nextUrl.searchParams;
-      const page = parseInt(searchQuery.get('page') || '1'); // Default to page 1 if not specified
-      const limit = parseInt(searchQuery.get('limit') || '10'); // Default limit to 10 if not specified
+      // const searchQuery = req.nextUrl.searchParams;
+      // const page = parseInt(searchQuery.get('page') || '1'); // Default to page 1 if not specified
+      // const limit = parseInt(searchQuery.get('limit') || '10'); // Default limit to 10 if not specified
 
       // Calculate skip value based on page and limit
-      const skip = (page - 1) * limit;
+      // const skip = (page - 1) * limit;
 
       // Fetch paginated contact data
       const contacts = await Contact.find()
-          .sort({ createdAt: -1 }) // Sort by createdAt in descending order
-          .skip(skip)
-          .limit(limit);
+          // .sort({ createdAt: -1 }) // Sort by createdAt in descending order
+          // .skip(skip)
+          // .limit(limit);
 
       // Get total count for pagination
-      const totalCount = await Contact.countDocuments();
+      // const totalCount = await Contact.countDocuments();
 
       // Calculate total number of pages
-      const totalPages = Math.ceil(totalCount / limit);
+      // const totalPages = Math.ceil(totalCount / limit);
 
       return NextResponse.json({
           msg: 'success',
           data: contacts,
-          totalCount,
-          currentPage: page,
-          totalPages, // Include total pages in the response
+          // totalCount,
+          // currentPage: page,
+          // totalPages, // Include total pages in the response
       });
   } catch (error) {
       console.error('Error fetching contact data:', error);
